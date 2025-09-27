2cedf141a53e2fd14fb2b8534f355336e0e15ad4
dubitable <dubitable97@gmail.com>
2025-09-27 20:09:06 +1200

    Fix build

--- src/routes/(app)/interests/book/[book_link]/+page.server.ts
+++ src/routes/(app)/interests/book/[book_link]/+page.server.ts
@@ -2,7 +2,7 @@ import type { EntryGenerator } from "./$types";
 import Books from "$lib/interests/books.json";
 
 export const entries: EntryGenerator = () => {
-  return Books.books.map((x) => {
+  return Books.books.filter(x => x.link).map((x) => {
     return {
       book_link: x.link,
     };
