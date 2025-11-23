3425d0b9323ed1729b09d647afbabea50642a3bd
dubitable <dubitable97@gmail.com>
2025-11-23 23:31:32 +1300

    Add quote and now page

---
+++ src/routes/(app)/now/+page.svelte
@@ -0,0 +1,13 @@
+<h1>Now!</h1>
+
+Last updated: 2025-11-23 As per<a href="https://nownownow.com/about"
+    >this now page</a
+>
+I must slavishly follow by adding a page that indicates what I'm up to. I doubt
+anyone cares but it's useful for myself to keep myself organised and thinking
+about what I'm up to. Currently learning
+<a href="https://gleam.run/">the Gleam programming language</a>
+by making something for the
+<a href="https://gleam-game-jam.pages.dev/">Gleam game jam</a>
+Publishing to
+<a href="https://gleamjam.dubitable.xyz">https://gleamjam.dubitable.xyz</a>
