e370ddd15eec72e50042d9a436d3a8ab7a6ae69e
dubitable <dubitable97@gmail.com>
2025-12-01 16:52:01 +1300

    Update now and add song

--- src/routes/(app)/now/+page.svelte
+++ src/routes/(app)/now/+page.svelte
@@ -1,13 +1,16 @@
 <h1>Now!</h1>
 
-Last updated: 2025-11-23 As per<a href="https://nownownow.com/about"
-    >this now page</a
->
-I must slavishly follow by adding a page that indicates what I'm up to. I doubt
-anyone cares but it's useful for myself to keep myself organised and thinking
-about what I'm up to. Currently learning
-<a href="https://gleam.run/">the Gleam programming language</a>
-by making something for the
-<a href="https://gleam-game-jam.pages.dev/">Gleam game jam</a>
-Publishing to
-<a href="https://gleamjam.dubitable.xyz">https://gleamjam.dubitable.xyz</a>
+<p>
+    Last updated: 2025-12-01 As per<a href="https://nownownow.com/about"
+        >this now page</a
+    >
+    I must slavishly follow by adding a page that indicates what I'm up to. I doubt
+    anyone cares but it's useful for myself to keep myself organised and thinking
+    about what I'm up to.
+</p>
+
+<p>
+    Currently: <br />
+
+    I don't know...
+</p>
