f957198cddcd3f7168fd5f6da41e549a5ec8d8be
dubitable <dubitable97@gmail.com>
2026-02-28 05:54:51 +1300

    Update now page

--- src/routes/(app)/now/+page.svelte
+++ src/routes/(app)/now/+page.svelte
@@ -1,7 +1,7 @@
 <h1>Now!</h1>
 
 <p>
-    Last updated: 2026-02-12 <br />
+    Last updated: 2026-02-28 <br />
     As per
     <a href="https://nownownow.com/about"> this now page </a>
     I must slavishly follow by adding a page that indicates what I'm up to. I doubt
@@ -12,21 +12,10 @@
 <p>
     Currently: <br />
 
-    Falling down a pit of failure
+    Disconsolate
 </p>
 
-<p>
-    Playing a lot of <a href="https://momentum-mod.org/">Momentum Mod</a> which
-    is really fun but a little too addicting, rocket jumping is so good but it
-    does make me want to wall jump off of large buildings
-    <abbr title="In real life">IRL</abbr> which is probably an unhealthy impulse.
-</p>
-
-<p>
-    Stuck in a rut in general, I like to work on random things but I don't
-    really have motivation for anything; can't finish a lot of the things I want
-    to; can't start anything without finishing them first.
-</p>
+<p>Idk</p>
 
 <p>
     Want to set up an <a href="https://en.wikipedia.org/wiki/RSS">RSS</a> reader for
