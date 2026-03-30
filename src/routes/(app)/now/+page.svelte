e1717276f1f24c55ef6e57656c2933f809b1e194
dubitable <dubitable97@gmail.com>
2026-03-30 21:46:11 +1300

    Update now page

--- src/routes/(app)/now/+page.svelte
+++ src/routes/(app)/now/+page.svelte
@@ -1,7 +1,7 @@
 <h1>Now!</h1>
 
 <p>
-    Last updated: 2026-02-28 <br />
+    Last updated: 2026-03-30 <br />
     As per
     <a href="https://nownownow.com/about"> this now page </a>
     I must slavishly follow by adding a page that indicates what I'm up to. I doubt
@@ -10,15 +10,28 @@
 </p>
 
 <p>
-    Currently: <br />
-
-    Disconsolate
+    Wow I haven't updated this page in a while. If only I was doing something
+    worthwhile to add here.
 </p>
 
-<p>Idk</p>
+<p>
+    Still playing a bunch of <a href="https://momentum-mod.org/">Momentum Mod</a
+    > but I really need to not overdo it
+</p>
 
 <p>
-    Want to set up an <a href="https://en.wikipedia.org/wiki/RSS">RSS</a> reader for
-    reading people's swag blogs. Would also be nice to setup my own RSS feed for the
-    sake of it but idk.
+    Want to mess around with fancy SVG stuff more as well, maybe I should add
+    notes on it or page(s) dedicated to experiments. I already have css
+    experiments under <a href="/css3d">css3d</a> so perhaps it would be good to unify
+    experiments in some cool way.
+</p>
+
+<p>
+    Been using an RSS reader but I don't have enough feeds added to it. Maybe I
+    can find some more cool ones; discovery is definitely the trickiest part of
+    the indie web. I don't have my own RSS feed, I guess I could have one for
+    posts I make but don't feel like it's worth it. I also like being able to
+    update posts after creating them rather than having them seem like immutable
+    texts. Mentally it really helps me to think that I'll be able to change
+    something if I'm not totally happy with it.
 </p>
