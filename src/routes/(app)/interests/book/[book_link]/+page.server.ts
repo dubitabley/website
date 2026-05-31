97de3c6f7905f55f4cf7b846923a2e20735d58c1
dubitable <dubitable97@gmail.com>
2026-05-31 22:53:51 +1200

    Clean up warnings/errors

--- src/routes/(app)/interests/book/[book_link]/+page.server.ts
+++ src/routes/(app)/interests/book/[book_link]/+page.server.ts
@@ -2,9 +2,11 @@ import type { EntryGenerator } from "./$types";
 import Books from "$lib/interests/books.json";
 
 export const entries: EntryGenerator = () => {
-  return Books.books.filter(x => x.link).map((x) => {
-    return {
-      book_link: x.link,
-    };
-  });
+    return Books.books
+        .filter((x) => x.link)
+        .map((x) => {
+            return {
+                book_link: x.link!,
+            };
+        });
 };
