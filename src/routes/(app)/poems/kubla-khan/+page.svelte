94e3881987a8dd3a1832cc4c09c585495a16bcde
dubitable <dubitable97@gmail.com>
2026-06-01 21:18:44 +1200

    Add kubla khan to poem

---
+++ src/routes/(app)/poems/kubla-khan/+page.svelte
@@ -0,0 +1,84 @@
+<h1>Kubla Khan</h1>
+<h3>by Samuel Taylor Coleridge</h3>
+
+<p>I've been listening to the song 'Xanadu' recently. No not that one.</p>
+<p>
+    It's <a href="https://youtu.be/p3xX-g4OMss">this one by Alex Glasgow</a>
+    about Xanadu as a utopian place. To me, it was basically a mythological place
+    like Timbuktu that sounds funny. However like Timbuktu it's actually a real place
+    but it's kind of interesting that this association exists. This originates from
+    this poem by Samuel Taylor Coleridge who you may remember from famous poems like
+    'The Rime of the Ancient Mariner' and 'Christabel'. Perhaps I should watch the
+    movie
+    <a href="https://en.wikipedia.org/wiki/Xanadu_(film)">Xanadu</a> at some
+    point but apparently it's an atrocious film and I don't watch many movies at
+    the best of times. It would be additionally cool to visit the real
+    <a href="https://en.wikipedia.org/wiki/Shangdu">Shangdu</a> but I would probably
+    require additional reasons for it rather than it being cool.
+</p>
+<p>
+    I will reproduce the poem here from <a
+        href="https://en.wikisource.org/wiki/Oxford_Book_of_English_Verse_1250-1900/Kubla_Khan"
+        >Wikisource</a
+    >. It is beautifully written and flows exquisitely so I hope any readers
+    enjoy.
+</p>
+<pre>
+    In Xanadu did Kubla Khan
+    A stately pleasure-dome decree:
+    Where Alph, the sacred river, ran
+    Through caverns measureless to man
+    Down to a sunless sea.
+    So twice five miles of fertile ground
+    With walls and towers were girdled round:
+    And there were gardens bright with sinuous rills
+    Where blossom'd many an incense-bearing tree;
+    And here were forests ancient as the hills,
+    Enfolding sunny spots of greenery.
+
+    But O, that deep romantic chasm which slanted
+    Down the green hill athwart a cedarn cover!
+    A savage place! as holy and enchanted
+    As e'er beneath a waning moon was haunted
+    By woman wailing for her demon-lover!
+    And from this chasm, with ceaseless turmoil seething,
+    As if this earth in fast thick pants were breathing,
+    A mighty fountain momently was forced;
+    Amid whose swift half-intermitted burst
+    Huge fragments vaulted like rebounding hail,
+    Or chaffy grain beneath the thresher's flail:
+    And 'mid these dancing rocks at once and ever
+    It flung up momently the sacred river.
+    Five miles meandering with a mazy motion
+    Through wood and dale the sacred river ran,
+    Then reach'd the caverns measureless to man,
+    And sank in tumult to a lifeless ocean:
+    And 'mid this tumult Kubla heard from far
+    Ancestral voices prophesying war!
+
+    The shadow of the dome of pleasure
+    Floated midway on the waves;
+    Where was heard the mingled measure
+    From the fountain and the caves.
+    It was a miracle of rare device,
+    A sunny pleasure-dome with caves of ice!
+
+    A damsel with a dulcimer
+    In a vision once I saw:
+    It was an Abyssinian maid,
+    And on her dulcimer she play'd,
+    Singing of Mount Abora.
+    Could I revive within me,
+    Her symphony and song,
+    To such a deep delight 'twould win me,
+    That with music loud and long,
+    I would build that dome in air,
+    That sunny dome! those caves of ice!
+    And all who heard should see them there,
+    And all should cry, Beware! Beware!
+    His flashing eyes, his floating hair!
+    Weave a circle round him thrice,
+    And close your eyes with holy dread,
+    For he on honey-dew hath fed,
+    And drunk the milk of Paradise.
+</pre>
