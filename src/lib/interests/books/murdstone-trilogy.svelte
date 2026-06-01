2f3760c4a84c51abe74a050b31d6815b2ca1e8f0
dubitable <dubitable97@gmail.com>
2026-06-01 12:00:23 +1200

    Add review for the murdstone trilogy

---
+++ src/lib/interests/books/murdstone-trilogy.svelte
@@ -0,0 +1,68 @@
+<p>
+    I am a sucker for metafiction. That's when the author talks directly to the
+    audience but as part of the text. I wrote my first review on this site about <a
+        href="/interests/book/french-lieutenants-woman"
+        >The French Lieutenant's Woman</a
+    > since I thought it was absolutely fascinating and it makes frequent use of this
+    technique. The power of this is that it can write about the story and characters
+    from a vantage point and force the reader to consider their assumptions about
+    the construction of the text.
+</p>
+<p>
+    The Murdstone Trilogy is a single book by Mal Peet, a children's book author
+    from England, about a children's book author from England who falls on hard
+    times and is directed to write formulaic fantasy novels to rescue his
+    fortunes. The text posits the issue as society not being interested in
+    reading thought out children's books that investigate gritty problems seen
+    throughout society. The book publishing industry is represented by a
+    manipulative and unscrupulous woman who encourages the protagonist Phillip
+    Murdstone into writing fantasy with the pure goal of selling more books. She
+    even gives him the standard fantasy formula based on the Tolkien books that
+    they need to have a world map and a cast replete with goblins, elves and
+    similar creatures.
+</p>
+<p>
+    However Murdstone struggles to write this and it's only via an alcohol
+    fueled dream of an encounter with a dwarf called Pocket that he's able to
+    write the book, although he believes it's Pocket writing the work in
+    exchange for a favour. I won't go into a detailed plot but essentially it
+    keeps costing Murdstone more and more as he goes on writing, sucking him
+    into the fantasy world he writes about. Of course all fantasy texts need to
+    be trilogies which is where the name of the book originates. Even though the
+    books are extraordinarily successful, Murdstone is never able to take
+    satisfaction from it - instead it fuels unhealthy spending habits.
+</p>
+<p>
+    Is this elitest? Snobbishly looking down on fantasy books as unable to tell
+    interesting or original stories? I think there's an element of that as is
+    inevitable with this kind of critique. However I was reading some popular
+    fantasy books recently and it is striking how some of the criticism does
+    strike home. It was supremely readable but I felt like it was just events
+    happening to characters rather than having interesting themes or elements.
+    There was some interesting worldbuilding that did diverge from the Tolkien
+    tradition in some ways, I think modern fantasy is more influenced by Brandon
+    Sanderson and other more recent writers. It was the first book of a trilogy
+    and really did seem just like a setup book and didn't end with much of a
+    resolution but more of an invitation to read the next book - bluntly setting
+    up the main conflict in the second book so transparently that I could
+    basically predict the overarching plot of the second and third book. I won't
+    name it but I do think there should be more of an artistic element to a book
+    and it failed to live up to that, instead just being a collection of popular
+    tropes for consumption.
+</p>
+<p>
+    The Murdstone Trilogy isn't quite metafiction but it does feel like it's not
+    far from directly talking to the reader at places. It is as if Mal Peet is
+    telling the reader: 'This is what I could be', immensely successfuly but
+    also soulless in a way that destroys me. It's not so self-gratifying as to
+    be obnoxious, he reserves as much criticism for the author in his book as
+    anyone else as Murdstone is fairly irascible and foolish in his own right.
+    He self-destructs, leading to a depressing ending but I don't think it could
+    or should have neded any differently.
+</p>
+<p>
+    I really enjoyed The Murdstone Trilogy - I would encourage you to try to
+    think about how fantasy books can explore interesting topics in a way that's
+    constructive rather than just being cheap escapism. I do still love fantasy
+    when done right.
+</p>
