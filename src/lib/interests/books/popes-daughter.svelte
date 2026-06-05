2d8bb251f092653205da5846d3b3c5d954c486b9
dubitable <dubitable97@gmail.com>
2026-06-05 23:48:49 +1200

    Add new book

---
+++ src/lib/interests/books/popes-daughter.svelte
@@ -0,0 +1,74 @@
+<p>
+    This novel was an interesting examination of Lucrezia Borgia, the daughter
+    of Pope Alexander VI, who was around in the 15th and 16th century. I enjoyed
+    it as an examination of both history and historiography, the study of
+    history, but the actual story behind Lecrezia's actions didn't feel engaging
+    to me. It's detailed as a kind of redemption story for Lucrezia, positing
+    itself as a different telling of the history to be sympathetic to her in
+    opposition to mainstream historic recounting which is condemning of her
+    behaviour. For some reason I am not an expert on 15th-16th century Vatican
+    politics so I didn't come into this with any strong pre-determined opinions
+    on any of the characters within.
+</p>
+
+<p>
+    The text works by recounting disparate events, some of which are based
+    entirely on historical accounts and others are inferred based on the things
+    that are known of the people that inhibit the text. I think the author
+    deliberately tries to instill something of the nature of the people he is
+    trying to convey, both to show their motivations and characters and keep a
+    healthy chronological flow for them. It protrays a debaucherous, corrupt and
+    deceitful Vatican and wider Europe where the ruling classes are all trying
+    to screw and murder each other before they get screwed or murdered first.
+    Lecrezia is portrayed as a survivor in this context where she has to survive
+    despite this atmosphere and manages to do so with aplomb, navigating all
+    that and leaving dead husbands behind to care for the people.
+</p>
+<p>
+    I think the book works if you read into its analysis a kind of meta-analysis
+    on how historians weave narratives and how this counternarrative is also
+    supported by the available information. Obviously historical readings of
+    these events is tricky since there isn't a clear detail on the motivations
+    for surviving documents and writings for the time so the biases of certain
+    people at the time can influence our modern portrayals and ideas of these
+    people. This can often lead people to thinking of history as being amorphous
+    and subjective to an extent - similar to postmodernist thinking. I tend to
+    prefer a kind of absolute truth way of operating although I do enjoy
+    postmodernist works.
+</p>
+<p>
+    Lecrezia is often maligned - as I suppose many prominent women are in
+    history which reminds me of this simpsons gag:
+</p>
+
+<img
+    width="100%"
+    src="https://simpsonswiki.com/wiki/File:Bart%27s_Girlfriend_Marquee.png"
+    alt="Simpsons billboard saying 'First Church of Springfield' 'Evil women in history: From Jezebel to Janet Reno'"
+/>
+<p>
+    The point is that Women are supposed to be seen as objects without agency to
+    be acted upon rather than. Even women who are exalted in history have to
+    overcome extreme barriers to achieve what they can. For example Sophie
+    Germain who did some incredible work on number theory in mathematics had to
+    do much of her work under a pseudonym and through correspondence with the
+    pre-eminent mathematicians of the time. Lecrezia is supposed to be
+    manipulative and seducing men in order to gain personal advantage and
+    assisting her father and brother in their selfish quests for power. The book
+    instead frames their activities as the malicious ones while she is less
+    active, more at the whims of a distasteful world. I think this tends to
+    reduce her own contributions in the ongoings. I'm sure she participated in
+    some of the seedy and dark going ons to some extent although the exact
+    amounts could be disputed. I could believe some of the other allegations may
+    be exaggerated in an attempt to defame her by enemies of her and her family.
+</p>
+
+<p>
+    Overall I enjoyed the book and it was very readable. Perhaps it would've
+    been more elegant and exquisite in the native Italian rather than the
+    translated English. It can also be read as a parallel to the modern
+    politicking, especially in the context of Italian politics since the author
+    was quite a prominent campaigner in that regard. I have little knowledge of
+    that so won't comment on it but I can see how the kinds of attitudes and
+    beliefs about the world could be seen in the modern context.
+</p>
