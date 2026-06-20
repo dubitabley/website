45327e888572b65306cde1d7a75ab2e225d2e85a
dubitable <dubitable97@gmail.com>
2026-06-20 15:59:03 +1200

    Add blog aggregator aggregator

---
+++ src/routes/(app)/site-aggregator-aggregator/+page.svelte
@@ -0,0 +1,14 @@
+<h1>List of list of blog aggregators</h1>
+
+<ul>
+    <li>
+        <a href="https://bstn.info/list-of-blog-aggregators">
+            bstn.info - List of Blog Aggregators
+        </a>
+    </li>
+    <li>
+        <a href="https://baccyflap.com/rsp/"
+            >baccyflap.com's ringspace - list of webrings</a
+        >
+    </li>
+</ul>
