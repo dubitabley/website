0c5fbb7994fd0034eb1251a4edf8a0526b6f5723
dubitable <dubitable97@gmail.com>
2026-06-20 13:51:39 +1200

    Add light-dark to transpilation exclude so it compiles correctly

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,14 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
+import { Features } from "lightningcss";
 
 export default defineConfig({
-  plugins: [sveltekit()],
+    plugins: [sveltekit()],
+    css: {
+        lightningcss: {
+            // https://github.com/vitejs/vite/issues/21911 this sucks - don't transform light-dark
+            // https://lightningcss.dev/transpilation.html#feature-flags
+            exclude: Features.LightDark,
+        },
+    },
 });
