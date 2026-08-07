dc283bdabb13b41c5da98fe510aa9169b2670948
dubitable <dubitable97@gmail.com>
2026-08-07 21:44:19 +1200

    Add new post about internet drama

---
+++ src/routes/(app)/posts/internet-drama/+page.svelte
@@ -0,0 +1,69 @@
+<h1>Internet Drama!!</h1>
+
+<p>
+    Internet drama is one of those things that are generally unhealthy but I
+    can't resist paying attention to anyway. I just wanted to rant about it here
+    to get some words out about the issues with it, mainly in the context of
+    speedrunning drama that happened recently since it's relatively harmless
+    compared to some of the other stuff that gets labelled under drama but it is
+    instructive for some of the principles people would be served to follow in
+    my opinion.
+</p>
+
+<ul>
+    <li>Don't worship people</li>
+    <li>Don't abuse people</li>
+    <li>Don't make things up</li>
+</ul>
+<p>Let's go through each of these points.</p>
+
+<h3>Don't worship people</h3>
+<p>
+    This isn't just about having idols or people that you look up to or respect.
+    Worhipping is when it crosses over to where you tend to support people
+    regardless of the evidence or believe they can do no wrong. This can lead to
+    all sorts of fanatical or unhealthy behaviour but is disturbingly common.
+    The reverse of this but in some ways even less healthy is to relentlessly
+    hate on someone, which tends to be even worse. If this were a proper proper
+    essay I would probably give an example or two and go into some of the
+    negative behaviours exhibited but I don't really want to go into specifics
+    because it feels like picking a side. <a href="https://youtu.be/Jdi5MkFWOx8"
+        >This video by Uncle Dane</a
+    > of all people is a useful video on not worshipping youtubers. In terms of speedrunners,
+    remember that being good at playing a video game fast doesn't necessarily make
+    them the arbiter of all things to do with that video game.
+</p>
+<h3>Don't abuse people</h3>
+<p>
+    I feel like this one should be common sense, you're just making the world a
+    worse place by insulting/sending people hate. Further to this is you should
+    consider how common comments are. If someone's already received 1000
+    messages about something, then sending another one isn't valuable criticism
+    and becomes a pile on. I suppose the key point here is to be empathetic and
+    remember that most people don't mean ill, even if it might appear so. Try to
+    at least understand other peoples' perspective before casting judgement.
+</p>
+<h3>Don't make things up</h3>
+<p>
+    This is really important since a lot of problems result from basic
+    misunderstanding or miscommunication between people or communities. Trying
+    to understand the facts of what has occurred is crucial to actually building
+    a shared reality which is important for resolving issues. If someone is
+    being accused of things they didn't do it makes them automatically defensive
+    and refuse to listen to legitimate criticism. It can also cause a minor
+    incident to be blown way out of proportion when rumours proliferate and make
+    the whole environment more toxic.
+</p>
+
+<p>
+    In general, I believe 'drama communities' which are groups of people only
+    interested in controversy and conflict tend to be really nasty. They cause
+    an influx of people unfamiliar with events to brigade and impose their views
+    on things which can only increase toxicity.If you are part of these
+    communities please reconsider whether it's good for them or even more
+    importantly whether it's good for you.
+</p>
+<p>
+    I am worried this is vague-posting a bit, but I do see the appeal. Thanks
+    for reading, if you have any thoughts please send them in I guess.
+</p>
