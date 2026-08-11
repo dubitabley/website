4876ac7873fa86192419ef876a5d76308b64fe7d
dubitable <dubitable97@gmail.com>
2026-08-12 00:42:13 +1200

    Fix build by disabling crawling in prerender config

--- svelte.config.js
+++ svelte.config.js
@@ -3,11 +3,15 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(),
+    preprocess: vitePreprocess(),
 
-  kit: {
-    adapter: adapter(),
-  },
+    kit: {
+        adapter: adapter(),
+        prerender: {
+            // can't do this because the at:// links don't work
+            crawl: false,
+        },
+    },
 };
 
 export default config;
