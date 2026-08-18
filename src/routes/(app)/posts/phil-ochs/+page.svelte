469e6f2ed20ae4f031395e2f97b100fb5bcdf0e9
dubitable <dubitable97@gmail.com>
2026-08-19 00:49:56 +1200

    Add phil ochs post

---
+++ src/routes/(app)/posts/phil-ochs/+page.svelte
@@ -0,0 +1,129 @@
+<svelte:head>
+	<link rel="site.standard.document" href="at://did:plc:fe4gbnsn7lm6kjq3behi5t2h/site.standard.document/3mteaw74zxc24"/>
+</svelte:head>
+<script>
+    import Footnote from "$lib/components/post/footnote.svelte";
+    import Post from "$lib/components/post/post.svelte";
+    import Quote from "$lib/components/quote.svelte";
+</script>
+
+<Post>
+    <h2>Phil Ochs</h2>
+    <p>Why Phil Ochs still lingers in the consciousness</p>
+    <p>
+        I recently watched <a href="https://youtu.be/Hvr80mzXNiA">this video</a>
+        by Abigail Devoe about the Phil Ochs album 'Rehearsals for Retirement'<Footnote
+            >They also have a website with their own posts which is really
+            awesome! <a
+                href="https://www.abigaildevoe.com/post/rehearsals-for-retirement"
+                >Here's a web version of the video.</a
+            ></Footnote
+        >. I enjoyed it and believe it represents the history and beliefs of
+        Ochs as well as the USA in the 60s but it's hard to totally convey why
+        he's still thought of. He wasn't the greatest singer, lyricist or most
+        prolific but I believe he was the most earnest. He gave his time, money,
+        soul and hopes to the changes in America and eventually it destroyed
+        him.
+    </p>
+    <Quote by="Phil Ochs" from="Crucifixion">
+        The child was created to the slaughterhouse he's led<br /> So good to be alive
+        when the eulogy is read
+    </Quote>
+    <p>
+        It's easy to feel like the world as they knew it was coming to an end in
+        1968. Martin Luther King and Robert Kennedy had been assassinated in the
+        USA. In France, May 68 there was a massive set of protests that came
+        close to revolution. Similarly in Japan there were enormous university
+        campus protests along with other rebellions in other countries. With a
+        pointless war going on, an old corrupt Republican president and a
+        feckless, morally hopeless Democratic opposition it was hard to see how
+        the world could continue as it was. Surely the world would collapse if
+        there was no momentous world-changing event blowing it up?
+    </p>
+    <Quote by="Phil Ochs" from="The War Is Over">
+        Drums are drizzling on a grain of sand<br /> Fading rhythms of a fading land
+    </Quote>
+    <p>
+        Phil Ochs called himself a topical singer but the topics are still just
+        as salient as then. Racism, poverty and war are still certainly with us;
+        perhaps that's the secret of all writers throughout history. Write about
+        what you see and know, and the themes in it will echo throughout time
+        and place. Phil can write about William Moore, a man taking a letter
+        across the American South demanding civil rights who was shot on the
+        road, or Paul Crump, a man in jail who killed someone and was sentenced
+        to death but seemed to be trying to turn his life around, even writing a
+        book, or John Henry Faulk, a radio host who was blacklisted during
+        McCarthyism and had to go to court to be able to broadcast again. Or one
+        of my favourite light-hearted songs <a
+            href="https://youtu.be/t2RsB73S8rI">Fifty Mile Hike</a
+        >
+        which is about a fitness program the president had instituted and the silliness
+        it had caused.<Footnote>
+            This is called a <a
+                href="https://en.wikipedia.org/wiki/Kennedy_march"
+                >Kennedy march</a
+            > but it was ultimately a futile effort to improve the health of Americans.
+        </Footnote> The names may change but the ideas and themes still persist. Their
+        stories, like mythology that is passed down as folk stories, become fables
+        that we need to hear to change.
+    </p>
+
+    <Quote by="Phil Ochs" from="What Are You Fighting For">
+        If you'll win the wars at home, there'll be no fighting anymore
+    </Quote>
+
+    <p>
+        Yet Phil couldn't last. He went up against the world and failed and
+        seemed to feel a frustration with the American public since they didn't
+        care about what they should. As he said "If there's any hope for
+        America, it lies in a revolution, and if there's any hope for a
+        revolution in America, it lies in getting Elvis Presley to become Che
+        Guevara." To that end he tried to imitate him, wearing a gold lamé suit
+        and releasing a spoof album called Phil Ochs' Greatest Hits with the
+        tagline "50 Phil Ochs fans can't be wrong", playing off Elvis' "50
+        million Elvis fans can't be wrong".<Footnote
+            >Like many Elvis things, this wasn't originally his either. It
+            actually came from an old song from the 1920s called "50 million
+            Frenchment can't be wrong" which you can hear a version of <a
+                href="https://youtu.be/u-IP0DE2kTI">here by Sophie Tucker</a
+            >. That also got parodied in Beauty and the Beast of all things
+            where they say "50 Frenchmen can't be wrong".
+        </Footnote>
+        However his attempts at commercial success to appeal to the masses were doomed
+        to failure. Perhaps they just sounded inauthentic or unspecific and musical
+        and social tastes were changing in ways that weren't for him. A superstar
+        musician or someone with mass commercial appeal could have changed but Phil
+        was not that. After this time his decline was slow but inevitable, both as
+        a songwriter and mentally until he took his life in 1976.
+    </p>
+    <Quote by="Phil Ochs" from="Chords Of Fame">
+        God help the troubadour <br />Who tries to be a star
+    </Quote>
+    <p>
+        It sounds like he should be forgotten, just another minor songwriter to
+        be randomly seen every now and again in music history books as one of
+        many passing fancies in the folk revival era. However he's certainly not
+        forgotten, there's this <a href="https://youtu.be/xSOOBlnmeP4"
+            >absolutely haunting Youtube video about the days before Phil died</a
+        >. Further there's many tributes to Ochs, whether it's covers of his
+        songs or other songs about Phil Ochs (I'm partial to
+        <a href="https://youtu.be/NgQCqHYYodk">this one by Josh Joplin</a> but there's
+        many others I love). There's also people writing new versions of his songs,
+        which is mostly done with the song "Love Me, I'm a Liberal" to update the
+        references for modern times, whenever those times might be. His legacy lives
+        on, even if it's never going to be mainstream and perhaps that's the most
+        anyone can ask for. People still care about him enough to keep his memory
+        alive
+    </p>
+
+    <Quote by="Phil Ochs" from="When I'm Gone">
+        Can't live proud enough to die when I'm gone <br />
+        So I guess I'll have to do it while I'm here
+    </Quote>
+
+    <p>
+        Phil Ochs is my favourite musician, because of all the reasons given
+        previously. Some of his songs are genuinely amazing and moving. Maybe
+        you'll take an interest too, maybe not but I hope you at least care.
+    </p>
+</Post>
